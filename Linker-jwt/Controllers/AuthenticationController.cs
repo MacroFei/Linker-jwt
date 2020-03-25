fa42a439802d5921a459b30be7b8064238beb384
CCCMacro <44434429+MacroFei@users.noreply.github.com>
2020-03-25 16:08:26 +0800

    添加项目文件。

---
+++ Linker-jwt/Controllers/AuthenticationController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Linker_jwt.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Linker_jwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class AuthenticationController : Controller
+    {
+        private readonly IAuthenticateService _authService;
+        public AuthenticationController(IAuthenticateService authService)
+        {
+            this._authService = authService;
+        }
+        [AllowAnonymous]
+        [HttpPost, Route("requestToken")]
+        public ActionResult RequestToken([FromBody] LoginRequestDTO request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid Request");
+            }
+
+            string token;
+            if (_authService.IsAuthenticated(request, out token))
+            {
+                return Ok(token);
+            }
+
+            return BadRequest("Invalid Request");
+        }
+    }
+}
